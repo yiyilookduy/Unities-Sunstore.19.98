6933614dc6789d72a4336ade4848f0ab73e9cec6
YiYiLook <Yiyilookduy@gmail.com>
2019-09-08 23:26:06 +0700

    Refactor code

--- Unities Sunstore.19.98/BL/Features/LineForBump.cs
+++ Unities Sunstore.19.98/BL/Features/LineForBump.cs
@@ -8,7 +8,44 @@ namespace Unities_Sunstore._19._98.BL.Features
     {
         public void Do()
         {
+            
 
+            userChoice = Console.ReadKey();
+
+            Console.Clear();
+
+            if (userChoice.Key == ConsoleKey.D1)
+            {
+                Console.WriteLine("Enter word do you want (number)");
+
+                if (Int32.TryParse(Console.ReadLine(), out int numOfWord))
+                {
+                    Console.WriteLine("Enter line do you want (number)");
+
+                    if (Int32.TryParse(Console.ReadLine(), out int numOfLine))
+                    {
+                        List<string> listString = instance.GetRandomResult(numOfWord, numOfLine).ToList();
+                        StringBuilder result = new StringBuilder();
+
+                        result.Append("{");
+                        foreach (var flag in listString)
+                        {
+                            if (flag != listString.Last())
+                                result.Append(flag + "|");
+                            else result.Append(flag);
+
+                        }
+
+                        result.Append("}");
+
+                        Console.WriteLine(result.ToString());
+                    }
+                    else Console.WriteLine("Line is a number");
+
+                }
+
+                else Console.WriteLine("Word is a number");
+            }
         }
     }
 }
--- Unities Sunstore.19.98/Program.cs
+++ Unities Sunstore.19.98/Program.cs
@@ -10,14 +10,7 @@ namespace Unities_Sunstore._19._98
 {
     class Program
     {
-        [DllImport("user32.dll")]
-        internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
-
-        [DllImport("user32.dll")]
-        internal static extern bool CloseClipboard();
-
-        [DllImport("user32.dll")]
-        internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
+        private static ConsoleKeyInfo userChoice;
 
 
         static void Main(string[] args)
@@ -26,58 +19,28 @@ namespace Unities_Sunstore._19._98
             // Declare
             Console.WriteLine("Hello Unities for Sunstore.19.98!");
             Console.OutputEncoding = Encoding.UTF8;
+            userChoice = new ConsoleKeyInfo();
+
             var instance = RandomBL.Instance;
-            ConsoleKeyInfo userChoice = new ConsoleKeyInfo();
             Dictionary<ConsoleKey,IFeatures> dictionary = new Dictionary<ConsoleKey, IFeatures>();
 
-            dictionary.Add(ConsoleKey.A, new LineForBump());
+            dictionary.Add(ConsoleKey.D1, new LineForBump());
+            AskUserForSelectFeatures();
+
 
 
             while (userChoice.Key != ConsoleKey.Escape)
             {
-                Console.WriteLine("\n 1-- Get random string for comment up post");
-
-                userChoice = Console.ReadKey();
-
-                Console.Clear();
-            
-                if (userChoice.Key == ConsoleKey.D1)
-                {
-                    Console.WriteLine("Enter word do you want (number)");
-
-                    if (Int32.TryParse(Console.ReadLine(), out int numOfWord))
-                    {
-                        Console.WriteLine("Enter line do you want (number)");
-
-                        if (Int32.TryParse(Console.ReadLine(), out int numOfLine))
-                        {
-                            List<string> listString = instance.GetRandomResult(numOfWord, numOfLine).ToList();
-                            StringBuilder result = new StringBuilder();
-
-                            result.Append("{");
-                            foreach (var flag in listString)
-                            {
-                                if (flag != listString.Last())
-                                    result.Append(flag + "|");
-                                else result.Append(flag);
-
-                            }
-
-                            result.Append("}");
-
-                            Console.WriteLine(result.ToString());
-                        }
-                        else Console.WriteLine("Line is a number");
-                        
-                    }
-
-                    else Console.WriteLine("Word is a number");
-
-
-                    
-                }
+                dictionary[userChoice.Key].Do();
+                AskUserForSelectFeatures();
             }
 
+        }
+
+        static void AskUserForSelectFeatures()
+        {
+            Console.WriteLine("\n 1-- Get random string for comment up post");
+            userChoice = Console.ReadKey();
         }
     }
 }
